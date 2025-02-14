0592f61cefa5e5a74498ec55fb62a872e66f901a
Vitalii Valkov <vitalii@hautech.ai>
2025-02-14 13:49:20 +0100

    Add ThemeProvider

---
+++ components/ThemeProvider/src/index.tsx
@@ -0,0 +1,70 @@
+import { createContext, PropsWithChildren, useContext } from "react";
+
+export type ThemeType = {
+  foundation: {
+    cornerRadius: {
+      xs: number;
+      s: number;
+      m: number;
+      l: number;
+      xl: number;
+      xxl: number;
+    };
+    spacing: {
+      xs: number;
+      s: number;
+      m: number;
+      ml: number;
+      l: number;
+      xl: number;
+      xxl: number;
+      xxxl: number;
+    };
+    stroke: {
+      thin: number;
+      standard: number;
+      thick: number;
+      xthick: number;
+    };
+  };
+  palette: {
+    layout: {
+      surfaceLow: string;
+      surfaceMid: string;
+      onSurface: {
+        primary: string;
+        secondary: string;
+        tertiary: string;
+      };
+      strokes: string;
+      skrim: string;
+    };
+    actions: {
+      primary: string;
+      onOrimary: string;
+      secondary: string;
+      onSecondary: string;
+      neutral: string;
+      onNeutral: string;
+      success: string;
+      onSuccess: string;
+      error: string;
+      onError: string;
+    };
+  };
+};
+
+export const ThemeContext = createContext<ThemeType>({} as ThemeType);
+
+export const ThemeProvider = ({
+  theme,
+  children,
+}: PropsWithChildren<{ theme: ThemeType }>) => {
+  return (
+    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
