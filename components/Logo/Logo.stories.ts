3620ecbff098a36eaa2c86f88ca3c442eb08f4c6
Vitalii Valkov <vitalii@hautech.ai>
2025-02-15 00:02:06 +0100

    Add Logo

---
+++ components/Logo/Logo.stories.ts
@@ -0,0 +1,15 @@
+import { Logo } from './src';
+
+export default {
+    title: 'Graphics/Logo',
+    component: Logo,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {},
+};
+
+export const Main = {
+    args: {},
+};
