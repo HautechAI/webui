1f9e483e4a7ff8c37c898b0ddd4d4beca70ff6a0
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-03-04 21:27:38 +0400

    feat: popover

---
+++ components/Popover/src/Popover.types.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+import { PopoverPosition } from 'react-tiny-popover';
+
+export type PopoverProps = {
+    content: ({ close }: { close: () => void }) => ReactNode;
+    contentPositions?: PopoverPosition[];
+    trigger: () => ReactNode;
+};
