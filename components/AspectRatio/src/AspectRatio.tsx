32c463a89c726606e065648938b4501a93f123ad
Nik Kogan <hrustnik25@gmail.com>
2025-03-09 01:57:11 +0400

    add aspect ratio component

--- components/AspectRatio/src/AspectRatio.tsx
+++ components/AspectRatio/src/AspectRatio.tsx
@@ -1,28 +1,64 @@
 import { SegmentedControl, SegmentedControlProps } from '@hautechai/webui.segmentedcontrol';
 import { ArrowAltRightIcon } from '@hautechai/webui.icon';
-import { styled } from '@hautechai/webui.themeprovider';
+import { useTheme } from '@hautechai/webui.themeprovider';
+import { useCallback, useState } from 'react';
+import { Modal } from '@hautechai/webui.modal';
+import CustomRatio from './CustomRatio';
+import { SmallRatioBox } from './styles';
 
-const Box = styled.div<{ width: number; height: number }>`
-    height: ${({ height }) => height}px;
-    width: ${({ width }) => width}px;
-    border-color: ${({ theme }) => theme.palette.layout.onSurface.primary};
-    border-style: solid;
-    border-width: ${({ theme }) => theme.foundation.stroke.standard}px;
-    border-radius: ${({ theme }) => theme.foundation.cornerRadius.s}px;
-    margin-right: ${({ theme }) => theme.foundation.spacing.xs}px;
-`;
+export type AspectRatioProps = SegmentedControlProps & {
+    calculateBoxSize: (aspectRatio: string) => { width: number; height: number };
+    onAspectRatioChange: (aspectRation: string) => void;
+    onPressCustomRatio?: () => void;
+    onCheckAsDefault?: (aspectRation: string, checked: boolean) => void;
+};
 
-export const AspectRatio = (props: SegmentedControlProps) => {
+const aspectRatios = ['1:1', '2:3', '12:5'];
+
+export const AspectRatio = (props: AspectRatioProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalPosition, setModalPosition] = useState({ left: 0, top: 0 });
+    const theme = useTheme();
+
+    const onTabChange = useCallback(
+        (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+            if (index === 3) {
+                const rect = event.currentTarget.getBoundingClientRect();
+                setModalPosition({
+                    left: rect.right + theme.foundation.spacing.l,
+                    top: rect.top - 420, // todo check how modal position can be dynamic according to screen size and position of the element
+                });
+                setIsModalOpen(true);
+            } else {
+                setIsModalOpen(false);
+                props.onAspectRatioChange(aspectRatios[index]);
+            }
+            props.onTabChange?.(event, index);
+        },
+        [props.onTabChange],
+    );
+
     return (
-        <SegmentedControl
-            {...props}
-            options={[
-                { label: '1:1', leadingIcon: <Box width={16} height={16} /> },
-                { label: '2:3', leadingIcon: <Box width={13} height={16} /> },
-                { label: '12:5', leadingIcon: <Box width={16} height={13} /> },
-                { leadingIcon: <ArrowAltRightIcon /> },
-            ]}
-            whitespace="m"
-        />
+        <>
+            <SegmentedControl
+                {...props}
+                options={[
+                    { label: aspectRatios[0], leadingIcon: <SmallRatioBox width={16} height={16} /> },
+                    { label: aspectRatios[1], leadingIcon: <SmallRatioBox width={12} height={16} /> },
+                    { label: aspectRatios[2], leadingIcon: <SmallRatioBox width={16} height={12} /> },
+                    { leadingIcon: <ArrowAltRightIcon /> },
+                ]}
+                whitespace="m"
+                onTabChange={onTabChange}
+            />
+            <Modal
+                open={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                backdropStyle={{ backgroundColor: 'transparent' }}
+                contentPosition={{ left: modalPosition.left, top: modalPosition.top }}
+            >
+                <CustomRatio {...props} />
+            </Modal>
+        </>
     );
 };
