86ca2e3ebf5abd19243d52d51aba478e87eb10c3
Nik Kogan <hrustnik25@gmail.com>
2025-03-09 01:38:49 +0400

    create aspect ratio story

--- apps/storybook/stories/AspectRatio.stories.tsx
+++ apps/storybook/stories/AspectRatio.stories.tsx
@@ -4,14 +4,42 @@ export default {
     title: 'Compositions/Aspect Ratio',
     component: AspectRatio,
     parameters: {
-        layout: 'centered',
+        layout: 'fullscreen',
     },
     tags: ['autodocs'],
+    decorators: [
+        (Story: any) => (
+            <div style={{ display: 'flex', width: 'auto', marginTop: '500px' }}>
+                <Story />
+            </div>
+        ),
+    ],
 };
 
 export const Main = {
     args: {
         onTabChange: (event: React.MouseEvent<HTMLDivElement, MouseEvent>, selectedTab: number) =>
             console.log('Selected tab:', selectedTab),
+        onAspectRatioChange: (aspectRatio: string) => console.log('Selected aspect ratio:', aspectRatio),
+        onPressCustomRatio: () => console.log('Custom ratio pressed'),
+        onCheckAsDefault: (aspectRatio: string, checked: boolean) =>
+            console.log('Set as default:', aspectRatio, checked),
+        calculateBoxSize: (aspectRatio: string) => {
+            const [width, height] = aspectRatio.split(':').map(Number); // '7:9' => [7, 9]
+            const ratio = width / height;
+            const maxSide = 1024;
+
+            if (width >= height) {
+                return {
+                    width: maxSide,
+                    height: Math.round(maxSide / ratio),
+                };
+            } else {
+                return {
+                    width: Math.round(maxSide * ratio),
+                    height: maxSide,
+                };
+            }
+        },
     },
 };
