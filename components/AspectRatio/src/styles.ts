ceaedacdcc08010ec8a81616fa2e0c567884d44b
Nik Kogan <hrustnik25@gmail.com>
2025-03-10 19:27:50 +0400

    move aspect ratio file

--- components/AspectRatio/src/styles.ts
+++ components/AspectRatio/src/styles.ts
@@ -1,5 +1,5 @@
 import { styled } from '@hautechai/webui.themeprovider';
-import { maxSize } from './CustomRatio';
+import { maxPxSize } from './CustomRatio';
 
 export const OptionLabel = styled.div`
     padding-left: ${({ theme }) => theme.foundation.spacing.ml}px;
@@ -8,8 +8,8 @@ export const OptionLabel = styled.div`
 `;
 
 export const AspectRatioBoxContainer = styled.div`
-    min-width: ${({}) => maxSize + 2}px;
-    min-height: ${({}) => maxSize + 2}px;
+    min-width: ${({}) => maxPxSize + 2}px;
+    min-height: ${({}) => maxPxSize + 2}px;
     align-items: center;
     justify-content: center;
     display: flex;
