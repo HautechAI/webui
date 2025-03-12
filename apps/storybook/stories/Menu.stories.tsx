29890be99d92c6be9e87cf14b08c5f01b3a07eec
Nik Kogan <hrustnik25@gmail.com>
2025-03-12 22:42:35 +0400

    create menu story

---
+++ apps/storybook/stories/Menu.stories.tsx
@@ -0,0 +1,48 @@
+import { MoreIcon, UploadIcon } from '../../../components/Icon/src';
+import { Menu } from '../../../components/Menu/src';
+import { Typography } from '../../../components/Typography/src';
+
+export default {
+    title: 'Navigation/Menu',
+    component: Menu,
+    tags: ['autodocs'],
+    argTypes: {},
+    parameters: {
+        layout: 'centered',
+    },
+    decorators: [
+        (Story: any) => (
+            <div style={{ padding: '50px', backgroundColor: 'lightgrey' }}>
+                <Story />
+            </div>
+        ),
+    ],
+};
+
+const menuItems = [
+    { label: 'Home', leadingIcon: <UploadIcon size={20} />, onClick: () => console.log('Home clicked') },
+    { label: 'Profile', trailingIcon: <MoreIcon size={20} />, onClick: () => console.log('Profile clicked') },
+    { type: 'divider' },
+    {
+        custom: (
+            <Typography variant="LabelSmallRegular" color="actions.error">
+                Move to trash
+            </Typography>
+        ),
+        onClick: () => console.log('Move to trash clicked'),
+    },
+];
+
+export const Default = {
+    args: {
+        model: menuItems,
+    },
+};
+
+export const Popover = {
+    args: {
+        model: menuItems,
+        trigger: () => <div>Open Popover Menu</div>,
+        contentPositions: ['top'],
+    },
+};
