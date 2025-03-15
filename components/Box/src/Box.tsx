f8188cbc033eeb5e89b41f4aba77fd20c5ab26f8
Vitalii Valkov <vitalii@hautech.ai>
2025-03-15 12:25:36 +0100

    Change Box default display to flex

--- components/Box/src/Box.tsx
+++ components/Box/src/Box.tsx
@@ -2,90 +2,80 @@ import { css, styled, ThemeType } from '@hautechai/webui.themeprovider';
 import { PropsWithChildren } from 'react';
 
 const StyledBox = styled.div<Omit<BoxProps, 'icon'>>`
+    display: ${({ display }) => display ?? 'flex'};
+
     ${({ width }) =>
         width
             ? css`
                   width: ${width}px;
               `
             : ''}
-
     ${({ height }) =>
         height
             ? css`
                   height: ${height}px;
               `
             : ''}
-    
     ${({ maxWidth }) =>
         maxWidth
             ? css`
                   max-width: ${maxWidth}px;
               `
             : ''}
-
     ${({ maxHeight }) =>
         maxHeight
             ? css`
                   max-height: ${maxHeight}px;
               `
             : ''}
-
     ${({ theme, padding }) =>
         padding
             ? css`
                   padding: ${theme.foundation.spacing[padding]}px;
               `
             : ''}
-
     ${({ theme, paddingTop }) =>
         paddingTop
             ? css`
                   padding-top: ${theme.foundation.spacing[paddingTop]}px;
               `
             : ''}
-
     ${({ theme, paddingRight }) =>
         paddingRight
             ? css`
                   padding-right: ${theme.foundation.spacing[paddingRight]}px;
               `
             : ''}
-
     ${({ theme, paddingBottom }) =>
         paddingBottom
             ? css`
                   padding-bottom: ${theme.foundation.spacing[paddingBottom]}px;
               `
             : ''}
-
     ${({ theme, paddingLeft }) =>
         paddingLeft
             ? css`
                   padding-left: ${theme.foundation.spacing[paddingLeft]}px;
               `
             : ''}
-
     ${({ overflow }) =>
         overflow
             ? css`
                   overflow: ${overflow};
               `
             : ''}
-
-
     ${({ overflowX }) =>
         overflowX
             ? css`
                   overflow-x: ${overflowX};
               `
             : ''}
-
-${({ overflowY }) =>
+    ${({ overflowY }) =>
         overflowY
             ? css`
                   overflow-y: ${overflowY};
               `
-            : ''}
+            : ''};
 `;
 
 export type BoxProps = PropsWithChildren<{
@@ -101,6 +91,7 @@ export type BoxProps = PropsWithChildren<{
     overflow?: 'hidden' | 'visible' | 'scroll' | 'auto';
     overflowX?: 'hidden' | 'visible' | 'scroll' | 'auto';
     overflowY?: 'hidden' | 'visible' | 'scroll' | 'auto';
+    display?: 'flex' | 'block' | 'inline-block' | 'inline-flex';
 }>;
 
 export const Box = (props: BoxProps) => {
