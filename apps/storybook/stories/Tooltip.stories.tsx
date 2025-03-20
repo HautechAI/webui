d2807243cf2ce96bea84abb100e59f69daa26ce5
irina-the-developer <irina.the.developer@gmail.com>
2025-03-20 15:31:52 +0700

    Feat: Add Tooltip

---
+++ apps/storybook/stories/Tooltip.stories.tsx
@@ -0,0 +1,42 @@
+import { fn } from '@storybook/test';
+
+import { Box } from '../../../components/Box/src';
+import { Tooltip } from '../../../components/Tooltip/src';
+
+export default {
+    title: 'Data Display/Tooltip',
+    component: Tooltip,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    args: {
+        text: 'This is a tooltip. Tooltips are used to describe or identify an element.',
+        children: <div>This is a hoverable item</div>,
+    },
+    decorators: [
+        (Story: any) => (
+            <Box width={150}>
+                <Story />
+            </Box>
+        ),
+    ],
+};
+
+export const Small = {
+    args: {},
+};
+
+export const Medium = {
+    args: {
+        size: 'medium',
+    },
+};
+
+export const MediumWithLink = {
+    args: {
+        size: 'medium',
+        buttonLabel: 'Label',
+        onClick: fn() as any,
+    },
+};
