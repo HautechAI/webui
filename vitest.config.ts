3d285078150a067dd99415539e254a00a6907b38
Vitalii Valkov <vitalii@hautech.ai>
2025-08-11 02:30:44 +0200

    Fix vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,19 @@
+import linaria from '@wyw-in-js/vite';
 import { defineConfig } from 'vitest/config';
-import { resolve } from 'path';
 
 export default defineConfig({
+    plugins: [
+        linaria({
+            include: ['**/*.{ts,tsx}'],
+            babelOptions: {
+                presets: ['@babel/preset-typescript', '@babel/preset-react'],
+            },
+        }),
+    ],
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: ['./vitest.setup.ts'],
-        include: ['components/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        include: ['components/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx,css}'],
     },
     esbuild: {
         jsx: 'automatic',
